dac1a0dbaef719271b0fd82da2f05745cc48f2ad
CodeVIP123 <145130284+CodeVIP123@users.noreply.github.com>
2025-01-26 14:03:48 +0530

    Fixed file structure

--- DrawinX/build.sh
+++
@@ -1,9 +0,0 @@
-nasm -f elf32 -o boot.o loader/boot.asm
-gcc -ffreestanding -m32 -c src/kernel.c -o kernel.o
-ld -m elf_i386 -Ttext 0x1000 -T linker/linker.ld -o kernel.bin boot.o kernel.o
-mv kernel.bin iso/boot/kernel.bin
-grub-mkrescue -o DrawinX.iso iso
-qemu-system-i386 -hda DrawinX.iso
-
-
-
--- DrawinX/clean.sh
+++
@@ -1,2 +0,0 @@
-rm -rd boot.o kernel.o iso/boot/kernel.bin DrawinX.iso
-clear
