34a6b80e5f1d8f62f32fafdc6384de761b74a8ee
CodeVIP123 <145130284+CodeVIP123@users.noreply.github.com>
2025-05-12 19:08:31 +0530

    Add files via upload

--- build.sh
+++ build.sh
@@ -4,5 +4,11 @@ nasm -f elf32 -o idts.o src/include/asm/idts.asm
 gcc -g -ffreestanding -m32 -c src/kernel.c -o kernel.o
 ld -m elf_i386 -Ttext 0x1000 -T linker/linker.ld -o kernel.bin boot.o kernel.o gdts.o idts.o
 mv kernel.bin iso/boot/kernel.bin
+dd if=/dev/zero of=storage.img bs=1M count=64
 grub-mkrescue -o DrawinX.iso iso
-qemu-system-i386 -hda DrawinX.iso
+qemu-system-i386 \
+  -cdrom DrawinX.iso \
+  -hda storage.img \
+  -m 4G\
+  -serial stdio
+
--- clean.sh
+++ clean.sh
@@ -1,2 +1,2 @@
-rm -rd boot.o kernel.o gdts.o idts.o iso/boot/kernel.bin DrawinX.iso
+rm -rd boot.o kernel.o gdts.o idts.o iso/boot/kernel.bin DrawinX.iso storage.img
 clear
